320680a87756e3c86e1b5a5aef425f6a49a1fdf3
Govind Rawat <gurucheeku91@gmail.com>
2024-10-08 11:13:24 +0530

    Create logic.py

---
+++ logic.py
@@ -0,0 +1,22 @@
+import tkinter as tk
+from tkinter import messagebox
+
+class CalculatorLogic:
+    def __init__(self, display):
+        self.display = display
+    
+    def on_button_click(self, char):
+        current_text = self.display.get()
+        self.display.delete(0, tk.END)
+        self.display.insert(0, current_text + char)
+
+    def calculate(self):
+        try:
+            result = str(eval(self.display.get()))  # Evaluate the expression entered
+            self.display.delete(0, tk.END)
+            self.display.insert(0, result)
+        except Exception as e:
+            messagebox.showerror("Error", "Invalid Input")
+
+    def clear_display(self):
+        self.display.delete(0, tk.END)
