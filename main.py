16b3abbce4128237d6406708278a2b1deea6297c
Govind Rawat <gurucheeku91@gmail.com>
2024-10-08 11:12:29 +0530

    Create main.py

---
+++ main.py
@@ -0,0 +1,13 @@
+from tkinter import Tk
+from ui import CalculatorUI
+
+if __name__ == "__main__":
+    root = Tk()
+    root.title("Simple Calculator")
+    root.geometry("400x500")
+    
+    # Create a calculator UI object and pass the root window
+    app = CalculatorUI(root)
+    
+    # Start the main event loop
+    root.mainloop()
